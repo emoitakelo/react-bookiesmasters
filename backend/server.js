fd09a548bef341a2511517f2ba360101e1abfbc9
Emoit <130679168+emoitakelo@users.noreply.github.com>
2025-07-28 17:56:49 +0300

    port varcel

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/predictions", predictionRoutes);
 
 // ✅ Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
