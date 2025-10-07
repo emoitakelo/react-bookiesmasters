89cec605939a324c2866344675225fc81bdb2d03
Emoit <130679168+emoitakelo@users.noreply.github.com>
2025-10-07 14:31:14 +0300

    tailwind intergration

--- frontend/src/components/FixtureCard.jsx
+++ frontend/src/components/FixtureCard.jsx
@@ -73,7 +73,7 @@ const FixtureCard = ({ fixture }) => {
           {/* Score / Status */}
           <div className="flex flex-col items-center w-1/3">
             <span className="text-gray-400 text-xs">{displayStatus}</span>
-            <span className="font-semibold text-lg text-white">
+            <span className="font-semibold text-medium text-white">
               {status?.short === "FT" ||
               status?.short === "1H" ||
               status?.short === "2H"
