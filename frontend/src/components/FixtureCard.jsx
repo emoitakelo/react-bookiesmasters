6ab526fd2bddee10cd12902bbe13ecbdbfc76ab1
Emoit <130679168+emoitakelo@users.noreply.github.com>
2025-10-07 15:58:08 +0300

    tailwind intergration

--- frontend/src/components/FixtureCard.jsx
+++ frontend/src/components/FixtureCard.jsx
@@ -340,7 +340,7 @@ const FixtureCard = ({ fixture }) => {
         </div>
 
         {/* Tip Badge */}
-<div className="flex justify-center mt-1">          
+<div className="flex justify-center ">          
                     <span className={`${tipBg} text-black w-7 h-7 flex items-center justify-center rounded-full text-sm shadow-md border border-teal-400`}>
 
              {/* className={`${tipBg} text-black font-bold w-7 h-7 flex items-center justify-center rounded-full text-sm`}
