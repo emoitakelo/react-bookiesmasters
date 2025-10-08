614d6b0c40e6329bb84a6f6176e814e772faf381
Emoit <130679168+emoitakelo@users.noreply.github.com>
2025-10-08 11:45:17 +0300

    tailwind intergration

--- frontend/src/components/fixtures/LeagueHeader.jsx
+++ frontend/src/components/fixtures/LeagueHeader.jsx
@@ -1,31 +1,10 @@
 const LeagueHeader = ({ league }) => {
   if (!league) return null;
-
   return (
-    <div className="flex items-start gap-2 mb-4">
-      {/* League logo */}
-      {league.logo && (
-        <img
-          src={league.logo}
-          alt={league.name}
-          className="w-7 h-7 object-contain"
-        />
-      )}
-
-      {/* League info */}
-      {/* <div className="flex flex-col items-start justify-center leading-snug">
-        <h2 className="font-bold text-base text-white m-0 p-0">
-          {league.name || "Unknown League"}
-        </h2> */}
-
-        {/* {league.country && 
-          <span className="text-sm text-gray-400 mt-[2px] block">
-            {league.country}
-          </span>
-        } */}
-      {/* </div> */}
+    <div className="flex items-center gap-2 mb-4">
+      {league.logo && <img src={league.logo} alt={league.name} className="w-6 h-6" />}
+      <h2 className="font-bold text-lg">{league.name || "Unknown League"}</h2>
     </div>
   );
 };
-
 export default LeagueHeader;
