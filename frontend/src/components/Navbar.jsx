f9d6a8dfb7b000af58775deeb532fdfd1d3d8d16
Emoit <130679168+emoitakelo@users.noreply.github.com>
2025-10-24 12:39:00 +0300

    nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -42,7 +42,7 @@ function Navbar() {
   className="md:hidden text-[#5eead4] hover:text-[#99f6e4] focus:outline-none focus:ring-0"
   onClick={() => setMenuOpen(!menuOpen)}
 >
-  {menuOpen ? <X size={24} /> : <Menu size={24} />}
+  {menuOpen ? <X size={20} /> : <Menu size={20} />}
 </button>
 
       </div>
